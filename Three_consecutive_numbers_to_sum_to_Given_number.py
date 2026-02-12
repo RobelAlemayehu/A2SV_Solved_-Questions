4c9bd15ef04ceef1b76136cfaf19721131152c87
RobelAlemayehu <robelalemayehu838@gmail.com>
2026-02-12 10:11:49 +0300

    leetcode 2177

---
+++ Three_consecutive_numbers_to_sum_to_Given_number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sumOfThree(self, num: int) -> List[int]:
+        if num % 3 == 0:
+
+            num1 = num // 3
+            return [num1 - 1, num1, num1 + 1]
+
+            
+        return []
+        
