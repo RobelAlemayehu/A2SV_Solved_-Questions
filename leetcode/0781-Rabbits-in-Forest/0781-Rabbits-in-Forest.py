cccab2ec495c6842044913ba633640a6308bf502
Robel Alemayehu Gebreyohannes <robelalemayehu838@gmail.com>
2026-03-17 18:42:42 +0300

    Add solution for Rabbits in Forest

---
+++ leetcode/0781-Rabbits-in-Forest/0781-Rabbits-in-Forest.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        result = 0
+
+        count = Counter(answers)
+
+        print(count)
+
+        for answer, freq in count.items():
+            same_color = answer + 1
+
+            groups = (freq + answer)  // same_color
+
+            result += groups * same_color
+
+
+        return result
